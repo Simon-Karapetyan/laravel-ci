b978985b0370743cfea835463e576ac4a51a1d42
Simon Karapetyan <simon.karapetyan@esterox.am>
2023-03-16 10:19:31 +0400

    Updated test

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -24,4 +24,14 @@ class ExampleTest extends TestCase
         $num2 = 2;
         $this->assertTrue($num1 + $num2 === 7);
     }
+
+    /**
+     * A basic test example.
+     */
+    public function test_sum_of_the_two_numbers1(): void
+    {
+        $num1 = 8;
+        $num2 = 2;
+        $this->assertTrue($num1 + $num2 === 7);
+    }
 }
